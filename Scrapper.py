5d3e582926a44ccf5bbbdba5c50735b0fa03f283
VaynerAkaWalo <vaynerakawalo@gmail.com>
2024-04-11 22:41:50 +0200

    test 5

--- Scrapper.py
+++ Scrapper.py
@@ -39,7 +39,7 @@ def main():
                 # for order in orders:
                     # logger.info(",".join(list(map(str, order.data)))+"\n")
                 if orders.__len__() > 0:
-                    logger.info(orders[0].malformed)
+                    logger.info(str(orders[0]))
     except:
         pass
 
